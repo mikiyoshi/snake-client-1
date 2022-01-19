36e32ae61ba512e08967309c81ed36ec63c98f31
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-01-19 19:36:01 +0000

    Connection module

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require('net');
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address here,
+    port: 50541, // PORT number here,
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    // code that does something when the connection is first established
+    console.log(`message: Kokura`);
+  });
+  return conn;
+};
+module.exports = {
+  connect,
+};
